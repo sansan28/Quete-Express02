420f138dc36bbafa4c90e2e39a664d4d2fa920e2
sansan28 <sandra.taboada@sncf.fr>
2023-01-22 19:31:00 +0100

    Exo quete 03

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express(); 
 
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -20,6 +21,9 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/movies", movieHandlers.postMovie); 
+app.post("/api/users", userHandlers.postUser);
+
 
 app.listen(port, (err) => {
   if (err) {
